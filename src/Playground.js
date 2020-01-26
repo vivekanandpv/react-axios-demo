2946b78216c70a1ea270ad73f7488528a3a3bc58
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-26 21:33:35 +0530

    done

--- src/Playground.js
+++ src/Playground.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import axios from 'axios';
+
+const getTodos = async () => {
+  try {
+    let list = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+    console.log('Data', list);
+  } catch (error) {
+    console.error('Axios Error', error);
+  }
+};
 
 const Playground = () => {
+  getTodos();
   return (
     <React.Fragment>
       <h3>Learning Http using Axios</h3>
