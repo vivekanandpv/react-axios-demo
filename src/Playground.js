508ffc6754b862d6bb7277179aedeccaae9e7d5e
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-26 21:54:59 +0530

    done

--- src/Playground.js
+++ src/Playground.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const getTodos = async () => {
+const getData = async () => {
   try {
     let list = await axios.get('https://jsonplaceholder.typicode.com/todos/');
-    console.log('Data', list);
+    return list.data;
   } catch (error) {
     console.error('Axios Error', error);
   }
 };
 
 const Playground = () => {
-  getTodos();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const get = async () => {
+      setData(await getData());
+    };
+
+    get();
+  }, []);
+
   return (
     <React.Fragment>
-      <h3>Learning Http using Axios</h3>
+      <h3>Todo List</h3>
+      <hr></hr>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>User Id</th>
+            <th>Title</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            ? data.map((t, i) => {
+                return (
+                  <React.Fragment>
+                    <tr key={i}>
+                      <td>{t.id}</td>
+                      <td>{t.userId}</td>
+                      <td>{t.title}</td>
+                      <td>{t.completed ? 'Completed' : 'Pending'}</td>
+                    </tr>
+                  </React.Fragment>
+                );
+              })
+            : null}
+        </tbody>
+      </table>
     </React.Fragment>
   );
 };
